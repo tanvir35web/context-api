5dcc4935153e7d7f304a69739d41481185455b40
tanvir35web <islammdtanvirul@gmail.com>
2024-03-10 22:09:38 +0600

    old project setup deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
 import "./App.css";
-import Counter from "./components/Counter";
-import { useContext } from "react";
-import { CounterContextApi } from "./context/CounterContext";
-import ResetCounter from "./components/ResetCounter";
 
 function App() {
 
-  const counterContext = useContext(CounterContextApi)
 
     return (
         <div className="p-10">
-            <h1 className="text-4xl mb-5 font-bold text-center">Count is {counterContext.count}</h1>
-            <Counter />
-            <Counter />
-            <Counter />
-            <Counter />
-            <ResetCounter />
+           
         </div>
     );
 }
