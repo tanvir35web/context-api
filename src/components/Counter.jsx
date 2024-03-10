66364ddf6fc08b65d509e35391b0ce969ed57f15
tanvir35web <islammdtanvirul@gmail.com>
2024-03-10 22:04:13 +0600

    increase button deleted for testing purpose

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -6,14 +6,7 @@ const Counter = () => {
     const counterContext = useContext(CounterContextApi);
     return (
         <div className="text-center">
-            <button
-                onClick={() =>
-                    counterContext.setCount(counterContext.count + 1)
-                }
-                className="p-2 bg-gray-100 m-1 rounded-md hover:bg-green-200"
-            >
-                Increment
-            </button> 
+            
             <button
                 onClick={() =>
                     counterContext.setCount(counterContext.count - 1)
